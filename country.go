7fef6d5af4a1877e200765a47918790b809efa18
João Lopes <joaomlopes@futsalmanager.pt>
2020-06-27 01:24:25 +0100

    Added /all endpoint and the option to filter the response

--- country.go
+++ country.go
@@ -1,82 +1,122 @@
 package gocountries
 
 import (
-  "errors"
-  "fmt"
-  "net/http"
-  "io/ioutil"
-  "encoding/json"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-const baseURL = "https://restcountries.eu/rest/v1/%s"
+const baseURL = "https://restcountries.eu/rest/v2/%s"
 
 type Country struct {
-  Name           string
-  Capital        string
-  AltSpellings   []string
-  Relevance      string
-  Region         string
-  Subregion      string
-  Translations   map[string]string
-  Population     int32
-  LatLng         []float32
-  Demonym        string
-  Area           float32
-  Gini           float32
-  Timezones      []string
-  Borders        []string
-  NativeName     string
-  CallingCodes   []string
-  TopLevelDomain []string
-  Alpha2Code     string
-  Alpha3Code     string
-  Currencies     []string
-  Languages      []string
+	Name           string
+	Capital        string
+	AltSpellings   []string
+	Relevance      string
+	Region         string
+	Subregion      string
+	Translations   map[string]string
+	Population     int32
+	LatLng         []float32
+	Demonym        string
+	Area           float32
+	Gini           float32
+	Timezones      []string
+	Borders        []string
+	NativeName     string
+	CallingCodes   []string
+	TopLevelDomain []string
+	Alpha2Code     string
+	Alpha3Code     string
+	Currencies     []map[string]string
+	Languages      []map[string]string
+	NumericCode    string
+	Flag           string
+	RegionalBlocs  []RegionalBloc
+	Cioc           string
+}
+
+// RegionalBloc model
+type RegionalBloc struct {
+	Acronym       string
+	Name          string
+	OtherAcronyms []string
+	OtherNames    []string
 }
 
 func doRestcountriesCall(apiSuffix string) ([]byte, error) {
-  url := fmt.Sprintf(baseURL, apiSuffix)
-  res, err := http.Get(url)
-  if err != nil {
-    return []byte{}, err
-  }
-  defer res.Body.Close()
-  if res.StatusCode != 200 {
-    e := errors.New(fmt.Sprintf("Unexpected API status code %s", res.Status))
-    return []byte{}, e
-  }
-  body, err := ioutil.ReadAll(res.Body)
-  if err != nil {
-    return []byte{}, err
-  }
-  return body, nil
+	url := fmt.Sprintf(baseURL, apiSuffix)
+	res, err := http.Get(url)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		e := errors.New(fmt.Sprintf("Unexpected API status code %s", res.Status))
+		return []byte{}, e
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+	return body, nil
 }
 
-func CountriesByName(name string) ([]Country, error) {
-  resData, err := doRestcountriesCall(fmt.Sprintf("name/%s", name))
+func AllCountries(filterResponse string) ([]Country, error) {
+	apiSuffix := "all"
+	if len(filterResponse) > 0 {
+		apiSuffix = fmt.Sprintf("all?fields=%s", filterResponse)
+	}
 
-  if err != nil {
-    return nil, err
-  }
-  var c []Country
-  err = json.Unmarshal(resData, &c)
-  if err != nil {
-    return nil, err
-  }
-  return c, nil
+	resData, err := doRestcountriesCall(apiSuffix)
+
+	if err != nil {
+		return nil, err
+	}
+	var c []Country
+	err = json.Unmarshal(resData, &c)
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
 }
 
-func CountriesByCapital(name string) ([]Country, error) {
-  resData, err := doRestcountriesCall(fmt.Sprintf("capital/%s", name))
+// CountriesByName searches for countries by their name. It can be the native name or partial name
+func CountriesByName(name, filterResponse string) ([]Country, error) {
+	apiSuffix := fmt.Sprintf("name/%s", name)
+	if len(filterResponse) > 0 {
+		apiSuffix = fmt.Sprintf("name/%s?fields=%s", name, filterResponse)
+	}
+	resData, err := doRestcountriesCall(apiSuffix)
 
-  if err != nil {
-    return nil, err
-  }
-  var c []Country
-  err = json.Unmarshal(resData, &c)
-  if err != nil {
-    return nil, err
-  }
-  return c, nil
+	if err != nil {
+		return nil, err
+	}
+	var c []Country
+	err = json.Unmarshal(resData, &c)
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
 }
 
+// CountriesByCapital searches for countries with capital city matching 'name'
+func CountriesByCapital(name, filterResponse string) ([]Country, error) {
+	apiSuffix := fmt.Sprintf("capital/%s", name)
+	if len(filterResponse) > 0 {
+		apiSuffix = fmt.Sprintf("capital/%s?fields=%s", name, filterResponse)
+	}
+	resData, err := doRestcountriesCall(apiSuffix)
+
+	if err != nil {
+		return nil, err
+	}
+	var c []Country
+	err = json.Unmarshal(resData, &c)
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
